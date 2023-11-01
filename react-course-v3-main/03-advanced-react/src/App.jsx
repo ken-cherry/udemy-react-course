03bc379e4b882e7d4217e0506e7829ef209ea9a7
Ken Cherry <info@cherrywebdev.com>
2023-10-31 21:06:31 -0500

    Added extension

--- react-course-v3-main/03-advanced-react/src/App.jsx
+++ react-course-v3-main/03-advanced-react/src/App.jsx
@@ -1,9 +1,29 @@
-function App() {
+// import Navbar from "./tutorial/04-project-structure/starter/Navbar";
+// import Home from "./tutorial/04-project-structure/starter/Pages/Home";
+// import About from "./tutorial/04-project-structure/starter/Pages/About";
+// import { Home, About } from "./tutorial/04-project-structure/starter/Pages";
+
+import Example from "./tutorial/04-project-structure/starter/Example.jsx";
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <Home />
+//       <About />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+const App = () => {
   return (
-    <div className='container'>
-      <h2>Advanced React</h2>
+    <div className="container">
+      <Example />
+      <div>
+        <h2>hello</h2>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
