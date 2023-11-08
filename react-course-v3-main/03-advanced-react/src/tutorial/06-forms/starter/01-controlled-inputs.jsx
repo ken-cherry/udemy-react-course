06110dc1651fdaf9d09b2e7f9d5510cdcd0b5fb1
Ken Cherry <info@cherrywebdev.com>
2023-11-07 18:14:59 -0600

    leverage-javascript

--- react-course-v3-main/03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,47 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email);
+  };
+
+  return (
+    <form action="" className="form" onSubmit={handleSubmit}>
+      <h4>controlled inputs one</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          name=""
+          id="name"
+          className="form-input"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="email"
+          name=""
+          id="email"
+          className="form-input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <button type="submit" className="btn btn-block">
+        submit
+      </button>
+    </form>
+  );
 };
 export default ControlledInputs;
