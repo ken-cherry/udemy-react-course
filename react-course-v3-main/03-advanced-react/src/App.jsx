4015b437f75213ee8504e037eb7e51ac2587fc7d
Ken Cherry <info@cherrywebdev.com>
2023-11-17 11:11:20 -0600

    leverage-javascript

--- react-course-v3-main/03-advanced-react/src/App.jsx
+++ react-course-v3-main/03-advanced-react/src/App.jsx
@@ -1,31 +1,9 @@
-// import Navbar from "./tutorial/04-project-structure/starter/Navbar";
-// import Home from "./tutorial/04-project-structure/starter/Pages/Home";
-// import About from "./tutorial/04-project-structure/starter/Pages/About";
-// import { Home, About } from "./tutorial/04-project-structure/starter/Pages";
-
-import Example from "./tutorial/04-project-structure/starter/Example.jsx";
-
-import List from "./tutorial/05-leverage-javascript/starter/List.jsx";
-
-// function App() {
-//   return (
-//     <div className="container">
-//       <Home />
-//       <About />
-//     </div>
-//   );
-// }
-
-// export default App;
+import ControlledInputs from "./tutorial/06-forms/starter/01-controlled-inputs";
 
 const App = () => {
   return (
     <div className="container">
-      {/* <Example /> */}
-      <div>
-        <h2>Leverage Javascript</h2>
-        <List />
-      </div>
+      <ControlledInputs />
     </div>
   );
 };
--- react-course-v3-main/03-advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,6 +1,8 @@
+import avatar from "../../../assets/default-avatar.svg";
 import React from "react";
 export default function Person({ name, nickName, images }) {
-  const img = images && images[0] && images[0].small && images[0].small.url;
+  // const img = images && images[0] && images[0].small && images[0].small.url;
+  const img = images?.[0]?.small?.url ?? avatar;
   return (
     <div>
       <img src={img} alt={name} style={{ width: "50px" }} />
